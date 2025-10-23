823475c6505c27f78bdbb31992c00f3ffc7f1a91
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-10-23 13:08:29 +0200

    end

--- TypeCheck/Arrays&Functions/scripts/arraysFunc.js
+++ TypeCheck/Arrays&Functions/scripts/arraysFunc.js
@@ -1,8 +1,32 @@
 //Vai a prendere dal DOM quell'elemento tramite id
 let res = document.getElementById("res")
 let input = document.getElementById("in")
-
+let inBtn = document.getElementById("inBtn")
 //console.log("CIAO");
 
-res.textContent = "Ciao"
-res.textContent += " MONDO"
+inBtn.addEventListener("click", getInputAndAdd)
+
+//Funzioni
+function getInputAndAdd() {
+    let inVal = Number(input.value)
+
+    //guardia
+    if (isNaN(inVal)) {
+        res.textContent = "Eh no dai.."
+        return
+    }
+
+    console.log(typeof inVal);
+    console.log((inVal + 5));
+    console.log(typeof (inVal + 5));
+
+    res.textContent = inVal + ": " + (inVal + 5)
+}
+
+//Array - eterogenei e dinamici
+let array = [1, 2, 3, 4, 5, 6, 6]
+array.push(123)
+console.log(array)
+let elem = array.pop()
+array.pop()
+console.log(array)
