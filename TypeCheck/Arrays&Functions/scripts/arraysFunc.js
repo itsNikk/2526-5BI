b8534307c8747111c2ac8a9ac22acf044396935b
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-10-23 12:39:53 +0200

    going on

---
+++ TypeCheck/Arrays&Functions/scripts/arraysFunc.js
@@ -0,0 +1,8 @@
+//Vai a prendere dal DOM quell'elemento tramite id
+let res = document.getElementById("res")
+let input = document.getElementById("in")
+
+//console.log("CIAO");
+
+res.textContent = "Ciao"
+res.textContent += " MONDO"
