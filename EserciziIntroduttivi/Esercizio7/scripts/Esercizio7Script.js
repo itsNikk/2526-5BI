21101f4d7ec09e8eae2d716f5b15399736a1e2f6
iTzNikk <nicolo.buganza@tiscali.it>
2025-11-06 21:16:44 +0100

    es7 sol1

---
+++ EserciziIntroduttivi/Esercizio7/scripts/Esercizio7Script.js
@@ -0,0 +1,20 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 120]
+
+let btn = document.getElementById("sumBtn")
+let res = document.getElementById("res")
+
+btn.addEventListener("click", () => {
+
+    //SOl 1 for indicizzato tradizionale
+    let sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+    }
+
+    
+
+    res.textContent = sum
+
+
+})
+
