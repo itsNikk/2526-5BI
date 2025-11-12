1807bb6c8b61796cc005ab126ec3fc6543cc0e6c
iTzNikk <nicolo.buganza@tiscali.it>
2025-11-12 22:44:36 +0100

    Es11 basic logic added

--- EserciziIntroduttivi/Esercizio11/scripts/Esercizio11Script.js
+++ EserciziIntroduttivi/Esercizio11/scripts/Esercizio11Script.js
@@ -3,11 +3,33 @@ let subBtn = document.getElementById("sub")
 let counterElem = document.getElementById("counter")
 let maxNumElem = document.getElementById("max")
 
+let counter = 0;
+
+function updateStatus() {
+    counterElem.textContent = counter;
+
+    let max = Number(maxNumElem.value)
+
+    if (counter >= max) {
+        counterElem.textContent = "LIMITE RAGGIUNTO"
+        addBtn.disabled = true
+        subBtn.disabled = false
+    }
+
+    if (counter <= 0) {
+        counterElem.textContent = "LIMITE RAGGIUNTO"
+        subBtn.disabled = true
+        addBtn.disabled = false
+    }
+
+}
 
 addBtn.addEventListener("click", () => {
-    maxNumElem.textContent = Number(maxNumElem.textContent) + 1
+    counter++;
+    updateStatus();
 })
 
 subBtn.addEventListener("click", () => {
-    maxNumElem.textContent = Number(maxNumElem.textContent) - 1
+    counter--;
+    updateStatus();
 })
