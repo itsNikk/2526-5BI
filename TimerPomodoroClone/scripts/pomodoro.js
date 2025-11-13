8c41a77a0419a8777ffa25f816181e6fee1f7209
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-11-13 12:49:08 +0100

    Update pomodoro.js

--- TimerPomodoroClone/scripts/pomodoro.js
+++ TimerPomodoroClone/scripts/pomodoro.js
@@ -3,10 +3,12 @@ let pauseBtn = document.getElementById("pauseBtn")
 let resetBtn = document.getElementById("resetBtn")
 let timerElem = document.getElementById("timer")
 
+
 let tomato = {
     //25 min * 60 = 1500 secondi
     seconds: 1500,
-    running: false
+    running: false,
+    timer: null
 }
 
 function showTimer() {
@@ -14,21 +16,37 @@ function showTimer() {
     let mins = Math.floor(tomato.seconds / 60)
     let secs = tomato.seconds % 60
 
-    timerElem.textContent = mins + ":" + secs
+    if (secs < 10) {
+        secs = "0" + secs
+    }
 
+    if (mins < 10) {
+        mins = "0" + mins
+    }
+
+    timerElem.textContent = mins + ":" + secs
 }
 
 function tick() {
-    tomato.seconds--
-
-    showTimer()
+    //-- => tomato.s = tomato.s - 1
+    tomato.seconds--;
+    showTimer();
 }
 
 function start() {
     if (!tomato.running) {
-        setInterval(tick, 1000);
-        tomato.running = true
+        // setInterval => esegui parametro 1 ogni poaramtro 2 ms
+        tomato.timer = setInterval(tick, 1000);
+        tomato.running = true;
     }
 }
 
+function reset() {
+    tomato.seconds = 1500;
+    tomato.running = false;
+    clearInterval(tomato.timer)
+    showTimer();
+}
+
+resetBtn.addEventListener("click", reset)
 startBtn.addEventListener("click", start)
