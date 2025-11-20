8bef0414cebbae26e9d375a75d0e154dee728608
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-11-20 12:54:44 +0100

    Update es21.js

--- Es21/scripts/es21.js
+++ Es21/scripts/es21.js
@@ -13,16 +13,18 @@ function genBtn() {
 
     let newBtn = document.createElement("button")
 
-    newBtn.textContent = index++
+    //math.random()
+    //tofixed(2)
+    newBtn.textContent = Math.floor(Math.random() * 10) + 1
     newBtn.addEventListener("click", () => {
         totalSum += Number(newBtn.textContent)
         newBtn.remove()
         console.log(totalSum);
 
     });
-
+    index++;
     brnContainer.append(newBtn)
 
 }
-
+//quando chiamo setInterval(tiomeout) => restituisco un rif 
 generator = setInterval(genBtn, 1000)
