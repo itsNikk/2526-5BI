ba04882c6de53d1f26caedaf30984fec5c4c9178
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-12-01 15:31:26 +0100

    es1.3
    fatti 3 punti

--- Sportello1122025/esS1/scripts/esS1.js
+++ Sportello1122025/esS1/scripts/esS1.js
@@ -2,6 +2,10 @@ let form = document.getElementById("inForm")
 let testoElem = document.getElementById("testo")
 let creativitaElem = document.getElementById("creativita")
 let listaElem = document.getElementById("lista")
+let scuseFiltrateElem = document.getElementById("scuseFiltrate")
+
+let filtroElem = document.getElementById("filtro")
+let filtroNumElem = document.getElementById("filtroNum")
 
 let excusesList = []
 
@@ -10,6 +14,28 @@ let excusesList = []
     creatività:5
 }*/
 
+//Punto 3
+creativitaElem.addEventListener("input", () => {
+    scuseFiltrateElem.innerHTML = "";
+    for (elem of excusesList) {
+        if (elem.creativita >= filtroNumElem.value) {
+            scuseFiltrateElem.innerHTML += elem.testo + ":" + elem.creativita + "<br>"
+        }
+    }
+})
+
+//Punto2
+filtroElem.addEventListener("input", () => {
+    scuseFiltrateElem.innerHTML = "";
+    for (elem of excusesList) {
+        //filtro
+        if (elem.testo.toLowerCase().includes(filtroElem.value)) {
+            //mostro scusa nel DOM  
+            scuseFiltrateElem.innerHTML += elem.testo + ":" + elem.creativita + "<br>"
+        }
+    }
+})
+
 //reagiamo alla submit del form
 form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -17,7 +43,9 @@ form.addEventListener("submit", (e) => {
     excusesList.push(
         { testo: testoElem.value, creativita: creativitaElem.value }
     );
-
+    
+    form.reset()
+    
     //fai vedere tutte le scuse
     // textContent e innerHTML = 1) inserisce tseto puro (non interpreta HTML)
     // 2) InnerHTML inserisce testo ma interpreta anche i tag HTML
