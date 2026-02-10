c32b15663c66514d66e6a551b398019a6de811c9
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-02-10 08:47:48 +0100

    Init space station API Exercise

---
+++ NodeProjects/SpaceStationAPI/spaceStationData.js
@@ -0,0 +1,12 @@
+const BASE_URL = "http://localhost:3000"
+
+async function getSolarPanels() {
+    let response = await fetch(BASE_URL + "/station/status");
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json);
+    } else {
+        console.log("Errore HTTP: " + response.status);
+    }
+} 
