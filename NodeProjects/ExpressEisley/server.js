8eb296928feece7fc5ba780bc44d6808c77b0f71
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-03-12 13:45:52 +0100

    Mos Eisley

---
+++ NodeProjects/ExpressEisley/server.js
@@ -0,0 +1,87 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+let clienti = [
+    { id: 1, nome: "Han Solo", specie: "umano", crediti: 1500 },
+    { id: 2, nome: "Chewbecca", specie: "wookie", crediti: 900 },
+    { id: 3, nome: "Greedo", specie: "rodiano", crediti: 300 },
+    { id: 4, nome: "hammerhead", specie: "ithoriano", crediti: 200 }
+]
+
+let bevande = [
+    { id: 1, nome: " Corelian Ale", prezzo: 50, gradazione: 8 },
+    { id: 2, nome: " Juice", prezzo: 80, gradazione: 15 },
+    { id: 3, nome: " Meranzane Gold", prezzo: 120, gradazione: 8 },
+    { id: 4, nome: " Spotchka", prezzo: 200, gradazione: 20 }
+]
+
+let nextClientId = clienti.length + 1;
+
+//MW Logger
+app.use((req, res, next) => {
+    console.log("[CANTINA LOG] " + req.method + " " + req.url);
+    next()
+})
+
+//MW Guardia - se non hai la tessera non entri
+
+app.use("/clienti", (req, res, next) => {
+    //console.log("[MW Clienti] QUI");
+    const tessera = req.headers["x-tessera"]
+
+    //403 - Forbidden
+    if (!tessera) {
+        return res.status(403).json({ error: "Devi avere una tessera. Regola della cantina" })
+    }
+
+    next()
+
+
+})
+
+//Aggiungete un MW su /clienti: - legge un Header x-gettoni
+// leggere quel valore e inserirlo nella request come req.gettoni 
+// Aggiunge contesto
+app.use("/clienti", (req, res, next) => {
+    const gettoni = parseInt(req.headers["x-gettoni"])
+    console.log("[MW Clienti gettoni:]" + gettoni);
+
+    if (isNaN(gettoni)) {
+        req.gettoni = 0
+    } else {
+        req.gettoni = gettoni
+    }
+
+    next()
+
+})
+
+app.use("/clienti", (req, res, next) => {
+    if (req.method !== "POST" && req.method !== "PUT") {
+        return next()
+    }
+
+    //Se è post o put fare roba...
+    const nome = req.body.nome
+    const specie = req.body.specie
+    const crediti = req.body.crediti
+
+    //Validazione dell'input
+    if (!nome){
+        res.status().json()
+    }
+})
+
+
+//E' UNA ROTTA (RH)
+app.get("/clienti", (req, res) => {
+
+    res.json(clienti)
+})
+
+app.listen(3000, () => {
+    console.log("Cantina aperta sulla porta 3000");
+})
